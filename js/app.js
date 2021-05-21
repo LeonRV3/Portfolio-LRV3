6cccb996f3ec6e03f13f2e627eb093c54c003930
LeonR9K <leonfelipe222@gmail.com>
2021-05-21 00:29:12 -0500

    añadida funcionalidad al menu, cambio de colores y responsive

--- js/app.js
+++ js/app.js
@@ -1,6 +1,7 @@
 const botonAnvorguesa = document.querySelector('.menu-hamburguesa');
 const menu = document.querySelector('.menu-desplegado');
 const body = document.querySelector('.body');
+const seleccionMenu = document.querySelector(".lista-menu-2");
 
 botonAnvorguesa.addEventListener('click', () => {
 
@@ -12,4 +13,10 @@ botonAnvorguesa.addEventListener('click', () => {
         body.style.cssText = 'overflow : hidden';
     }
 
+});
+
+seleccionMenu.addEventListener('click', (e) => {
+    console.log("Seleccione una opcion del menu");
+    menu.style.display = 'none';
+    body.style.cssText = 'overflow : visible';
 });
